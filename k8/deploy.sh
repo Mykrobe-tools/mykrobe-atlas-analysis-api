887629ab3f9106269ce9e3dba7b24403ac56fc75
mthomsit <mark@makeandship.co.uk>
2019-04-24 10:42:09 +0100

    Skeleton deploy script

---
+++ k8/deploy.sh
@@ -0,0 +1,27 @@
+if [ -z $KUBE_TOKEN ]; then
+  echo "FATAL: Environment Variable KUBE_TOKEN must be specified."
+  exit ${2:-1}
+fi
+
+if [ -z $NAMESPACE ]; then
+  echo "FATAL: Environment Variable NAMESPACE must be specified."
+  exit ${2:-1}
+fi
+
+if [ -z $KUBERNETES_SERVICE_HOST ]; then
+  echo "FATAL: Environment Variable KUBERNETES_SERVICE_HOST must be specified."
+  exit ${2:-1}
+fi
+
+if [ -z $KUBERNETES_PORT_443_TCP_PORT ]; then
+  echo "FATAL: Environment Variable KUBERNETES_PORT_443_TCP_PORT must be specified."
+  exit ${2:-1}
+fi
+
+echo
+echo "Deploying Atlas"
+echo 
+
+echo
+echo "Deploying BIGSI"
+echo 
