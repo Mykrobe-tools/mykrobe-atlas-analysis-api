980426d659b2345dae89f5faadc70c6383176000
mthomsit <mark@makeandship.co.uk>
2019-04-26 10:44:28 +0100

    Deploy redis

--- k8/deploy.sh
+++ k8/deploy.sh
@@ -19,6 +19,34 @@ if [ -z $KUBERNETES_PORT_443_TCP_PORT ]; then
 fi
 
 echo
+echo "Deploying Redis"
+echo
+
+status_code=$(curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
+    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/apps/v1beta2/namespaces/$NAMESPACE/deployments/redis" \
+    -X GET -o /dev/null -w "%{http_code}")
+
+echo
+echo "Redis status: $status_code"
+echo
+
+if [ $status_code == 200 ]; then
+  echo
+  echo "Updating deployment"
+  echo
+  curl -H 'Content-Type: application/strategic-merge-patch+json' -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
+    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/apps/v1beta2/namespaces/$NAMESPACE/deployments/redis" \
+    -X PATCH -d @k8/redis-deployment.json
+else
+  echo
+  echo "Creating deployment"
+  echo
+  curl -H 'Content-Type: application/json' -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
+    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/apps/v1beta2/namespaces/$NAMESPACE/deployments" \
+    -X POST -d @k8/redis-deployment.json
+fi
+
+echo
 echo "Deploying Atlas"
 echo 
 
