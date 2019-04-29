21c7ca1fd246e801bb44e67e0033444ff87c630a
mthomsit <mark@makeandship.co.uk>
2019-04-29 16:32:25 +0100

    Initial json for BIGSI deploy

---
+++ k8/deploy-bigsi.sh
@@ -0,0 +1,43 @@
+if [ -z $KUBE_TOKEN ]; then
+  echo "FATAL: Environment Variable KUBE_TOKEN must be specified."
+  exit ${2:-1}
+fi
+
+if [ -z $NAMESPACE ]; then
+  echo "FATAL: Environment Variable NAMESPACE must be specified."
+  exit ${2:-1}
+fi
+
+if [ -z $KUBERNETES_SERVICE_HOST ]; then
+  echo "FATAL: Environment Variable KUBERNETES_SERVICE_HOST must be specified."
+  exit ${2:-1}
+fi
+
+if [ -z $KUBERNETES_PORT_443_TCP_PORT ]; then
+  echo "FATAL: Environment Variable KUBERNETES_PORT_443_TCP_PORT must be specified."
+  exit ${2:-1}
+fi
+
+status_code=$(curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
+    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/apps/v1beta2/namespaces/$NAMESPACE/configmaps/mykrobe-atlas-bigsi-env" \
+    -X GET -o /dev/null -w "%{http_code}")
+
+echo
+echo "BIGSI env config map status: $status_code"
+echo
+
+if [ $status_code == 200 ]; then
+  echo
+  echo "Updating BIGSI env config map"
+  echo
+  curl -H 'Content-Type: application/strategic-merge-patch+json' -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
+    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/apps/v1beta2/namespaces/$NAMESPACE/cnfigmaps/mykrobe-atlas-bigsi-env" \
+    -X PATCH -d @k8/bisgi/bigsi-service/mykrobe-atlas-bigsi-env.json
+else
+  echo
+  echo "Creating BIGSI env config map"
+  echo
+  curl -H 'Content-Type: application/json' -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
+    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/apps/v1beta2/namespaces/$NAMESPACE/deployments" \
+    -X POST -d @k8/bisgi/bigsi-service/mykrobe-atlas-bigsi-env.json
+fi
