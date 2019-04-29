9573d4ab4c10bbc8d31f06dd45c794b1f481fa8b
mthomsit <mark@makeandship.co.uk>
2019-04-29 17:09:02 +0100

    Bigsi

--- k8/deploy-bigsi.sh
+++ k8/deploy-bigsi.sh
@@ -18,6 +18,10 @@ if [ -z $KUBERNETES_PORT_443_TCP_PORT ]; then
   exit ${2:-1}
 fi
 
+curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
+    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/apps/v1beta2/namespaces/$NAMESPACE/configmaps" \
+    -X GET
+
 status_code=$(curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
     "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/apps/v1beta2/namespaces/$NAMESPACE/configmaps/mykrobe-atlas-bigsi-env" \
     -X GET -o /dev/null -w "%{http_code}")
