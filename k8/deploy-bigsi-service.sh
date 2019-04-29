cbb16f5bfed11107bf6020bd583c67f48d752658
mthomsit <mark@makeandship.co.uk>
2019-04-29 18:07:47 +0100

    Bigsi

--- k8/deploy-bigsi-service.sh
+++ k8/deploy-bigsi-service.sh
@@ -17,39 +17,26 @@ if [ -z $KUBERNETES_PORT_443_TCP_PORT ]; then
   echo "FATAL: Environment Variable KUBERNETES_PORT_443_TCP_PORT must be specified."
   exit ${2:-1}
 fi
-
-echo
-echo "Deploying Redis"
-echo
-
 status_code=$(curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
-    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/apps/v1beta2/namespaces/$NAMESPACE/deployments/redis" \
+    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/apps/v1beta2/namespaces/$NAMESPACE/configmaps/mykrobe-atlas-bigsi-env" \
     -X GET -o /dev/null -w "%{http_code}")
 
 echo
-echo "Redis status: $status_code"
+echo "BIGSI env config map status: $status_code"
 echo
 
 if [ $status_code == 200 ]; then
   echo
-  echo "Updating deployment"
+  echo "Updating BIGSI env config map"
   echo
   curl -H 'Content-Type: application/strategic-merge-patch+json' -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
-    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/apps/v1beta2/namespaces/$NAMESPACE/deployments/redis" \
-    -X PATCH -d @k8/redis-deployment.yaml
+    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/apps/v1beta2/namespaces/$NAMESPACE/configmaps/mykrobe-atlas-bigsi-env" \
+    -X PATCH -d @k8/bigsi/bigsi-service/mykrobe-atlas-bigsi-env.json
 else
   echo
-  echo "Creating deployment"
+  echo "Creating BIGSI env config map"
   echo
   curl -H 'Content-Type: application/json' -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
-    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/apps/v1beta2/namespaces/$NAMESPACE/deployments" \
-    -X POST -d @k8/redis-deployment.yaml
+    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/apps/v1beta2/namespaces/$NAMESPACE/configmaps" \
+    -X POST -d @k8/bigsi/bigsi-service/mykrobe-atlas-bigsi-env.json
 fi
-
-echo
-echo "Deploying Atlas"
-echo 
-
-echo
-echo "Deploying BIGSI"
-echo 
