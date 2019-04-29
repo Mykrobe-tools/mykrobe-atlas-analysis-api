060c8943664e3a1a72e27932499725745495d966
mthomsit <mark@makeandship.co.uk>
2019-04-29 18:15:18 +0100

    Bigsi

--- k8/deploy-bigsi-service.sh
+++ k8/deploy-bigsi-service.sh
@@ -21,6 +21,9 @@ status_code=$(curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
     "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/apps/v1beta2/namespaces/$NAMESPACE/configmaps/mykrobe-atlas-bigsi-env" \
     -X GET -o /dev/null -w "%{http_code}")
 
+pwd
+ls -la
+
 echo
 echo "BIGSI env config map status: $status_code"
 echo
