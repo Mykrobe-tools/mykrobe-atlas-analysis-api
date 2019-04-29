c3546e4ef99c589ba7c0322a027c62e839f060e1
mthomsit <mark@makeandship.co.uk>
2019-04-29 21:38:37 +0100

    Bigsi

--- k8/deploy-bigsi-service.sh
+++ k8/deploy-bigsi-service.sh
@@ -18,7 +18,7 @@ if [ -z $KUBERNETES_PORT_443_TCP_PORT ]; then
   exit ${2:-1}
 fi
 status_code=$(curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
-    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/apps/v1beta2/namespaces/$NAMESPACE/configmaps/mykrobe-atlas-bigsi-env" \
+    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/apps/v1/namespaces/$NAMESPACE/configmaps/mykrobe-atlas-bigsi-env" \
     -X GET -o /dev/null -w "%{http_code}")
 
 pwd
@@ -33,7 +33,7 @@ if [ $status_code == 200 ]; then
   echo "Updating BIGSI env config map"
   echo
   curl -H 'Content-Type: application/strategic-merge-patch+json' -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
-    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/apps/v1beta2/namespaces/$NAMESPACE/configmaps/mykrobe-atlas-bigsi-env" \
+    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/apps/v1/namespaces/$NAMESPACE/configmaps/mykrobe-atlas-bigsi-env" \
     -X PATCH -d @k8/bigsi/bigsi-service/mykrobe-atlas-bigsi-env.json
 else
   echo
@@ -41,6 +41,6 @@ else
   echo
 
   curl -H 'Content-Type: application/json' -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
-    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/apps/v1beta2/namespaces/$NAMESPACE/configmaps" \
+    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/apps/v1/namespaces/$NAMESPACE/configmaps" \
     -X POST -d @k8/bigsi/bigsi-service/mykrobe-atlas-bigsi-env.json
 fi
