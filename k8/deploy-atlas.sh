58f3129f685d273cde1693c2609ea90ea8f9a0f2
mthomsit <mark@makeandship.co.uk>
2019-04-30 16:48:34 +0100

    Atlas

--- k8/deploy-atlas.sh
+++ k8/deploy-atlas.sh
@@ -77,3 +77,29 @@ else
 fi
 
 echo
+
+# --------------------------------------------------------------
+
+status_code=$(curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
+    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/api/v1/namespaces/$NAMESPACE/configmaps/env" \
+    -X GET -o /dev/null -w "%{http_code}")
+
+echo
+echo "Atlas env config map status: $status_code"
+
+if [ $status_code == 200 ]; then
+  echo "Updating Atlas env config map"
+  echo
+  curl -H 'Content-Type: application/strategic-merge-patch+json' -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
+    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/api/v1/namespaces/$NAMESPACE/configmaps/env" \
+    -X PATCH -d @k8/atlas/env.json
+else
+  echo "Creating Atlas env config map"
+  echo
+
+  curl -H 'Content-Type: application/json' -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
+    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/api/v1/namespaces/$NAMESPACE/configmaps" \
+    -X POST -d @k8/atlas/env.json
+fi
+
+echo
