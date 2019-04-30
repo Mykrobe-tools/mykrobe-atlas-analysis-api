eb7c30d3dc50dbfb7841b09a7e7583f828c402fd
mthomsit <mark@makeandship.co.uk>
2019-04-30 17:20:54 +0100

    Atlas

--- k8/deploy-atlas.sh
+++ k8/deploy-atlas.sh
@@ -129,3 +129,55 @@ else
 fi
 
 echo
+
+# --------------------------------------------------------------
+
+status_code=$(curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
+    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/apps/v1beta1/namespaces/$NAMESPACE/deployments/mykrobe-atlas-analysis-api" \
+    -X GET -o /dev/null -w "%{http_code}")
+echo
+
+echo "Analysis API deployment status: $status_code"
+
+if [ $status_code == 200 ]; then
+  echo "Updating Analysis API deployment deployment"
+  echo
+  curl -H 'Content-Type: application/strategic-merge-patch+json' -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
+    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/apps/v1beta1/namespaces/$NAMESPACE/deployments/mykrobe-atlas-analysis-api" \
+    -X PATCH -d @k8/atlas/mykrobe-atlas-analysis-api.json
+else
+  echo "Creating Analysis API deployment deployment"
+  echo
+
+  curl -H 'Content-Type: application/json' -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
+    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/apps/v1beta1/namespaces/$NAMESPACE/deployments" \
+    -X POST -d @k8/atlas/mykrobe-atlas-analysis-api.json
+fi
+
+echo
+
+# --------------------------------------------------------------
+
+status_code=$(curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
+    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/apps/v1beta1/namespaces/$NAMESPACE/deployments/mykrobe-atlas-analysis-worker" \
+    -X GET -o /dev/null -w "%{http_code}")
+echo
+
+echo "Analysis worker deployment status: $status_code"
+
+if [ $status_code == 200 ]; then
+  echo "Updating Analysis worker deployment"
+  echo
+  curl -H 'Content-Type: application/strategic-merge-patch+json' -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
+    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/apps/v1beta1/namespaces/$NAMESPACE/deployments/mykrobe-atlas-analysis-worker" \
+    -X PATCH -d @k8/atlas/mykrobe-atlas-analysis-worker.json
+else
+  echo "Creating Analysis worker deployment"
+  echo
+
+  curl -H 'Content-Type: application/json' -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
+    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/apps/v1beta1/namespaces/$NAMESPACE/deployments" \
+    -X POST -d @k8/atlas/mykrobe-atlas-analysis-worker.json
+fi
+
+echo
