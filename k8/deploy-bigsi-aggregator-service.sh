bffebfc406eaeaa5ab3722782e5e587b994008b7
mthomsit <mark@makeandship.co.uk>
2019-04-30 10:52:08 +0100

    Bigsi aggregator

--- k8/deploy-bigsi-aggregator-service.sh
+++ k8/deploy-bigsi-aggregator-service.sh
@@ -128,3 +128,29 @@ else
 fi
 
 echo
+
+# --------------------------------------------------------------
+
+status_code=$(curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
+    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/extensions/v1beta1/namespaces/$NAMESPACE/ingresses/bigsi-aggregator-ingress" \
+    -X GET -o /dev/null -w "%{http_code}")
+
+echo
+echo "BIGSI aggregator ingress status: $status_code"
+
+if [ $status_code == 200 ]; then
+  echo "Updating BIGSI ingress service"
+  echo
+  curl -H 'Content-Type: application/strategic-merge-patch+json' -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
+    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/extensions/v1beta1/namespaces/$NAMESPACE/ingresses/bigsi-aggregator-ingress" \
+    -X PATCH -d @k8/bigsi/bigsi-aggregator-service/bigsi-ingress.json
+else
+  echo "Creating BIGSI ingress service"
+  echo
+
+  curl -H 'Content-Type: application/json' -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
+    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/extensions/v1beta1/namespaces/$NAMESPACE/services" \
+    -X POST -d @k8/bigsi/bigsi-aggregator-service/bigsi-ingress.json
+fi
+
+echo
