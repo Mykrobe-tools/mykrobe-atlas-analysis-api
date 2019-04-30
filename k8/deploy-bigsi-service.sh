00545ab3a948c3f0d39c3b06e25a851366052575
mthomsit <mark@makeandship.co.uk>
2019-04-30 08:06:01 +0100

    Bigsi

--- k8/deploy-bigsi-service.sh
+++ k8/deploy-bigsi-service.sh
@@ -24,12 +24,9 @@ status_code=$(curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
     "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/api/v1/namespaces/$NAMESPACE/persistentvolumes/pv-volume-for-mykrobe-atlas-bigsi" \
     -X GET -o /dev/null -w "%{http_code}")
 
-echo
 echo "BIGSI service pv volume: $status_code"
-echo
 
 if [ $status_code == 200 ]; then
-  echo
   echo "Updating BIGSI service pv volume"
   echo
 
@@ -37,7 +34,6 @@ if [ $status_code == 200 ]; then
     "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/api/v1/namespaces/$NAMESPACE/persistentvolumes/pv-volume-for-mykrobe-atlas-bigsi" \
     -X PATCH -d @k8/bigsi/bigsi-service/pv-volume.json
 else
-  echo
   echo "Creating BIGSI service pv volume"
   echo
 
@@ -52,19 +48,15 @@ status_code=$(curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
     "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/api/v1/namespaces/$NAMESPACE/persistentvolumeclaims/pv-claim-for-mykrobe-atlas-bigsi" \
     -X GET -o /dev/null -w "%{http_code}")
 
-echo
 echo "BIGSI service pv claim: $status_code"
-echo
 
 if [ $status_code == 200 ]; then
-  echo
   echo "Updating BIGSI service pv claim"
   echo
   curl -H 'Content-Type: application/strategic-merge-patch+json' -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
     "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/api/v1/namespaces/$NAMESPACE/persistentvolumeclaims/pv-claim-for-mykrobe-atlas-bigsi" \
     -X PATCH -d @k8/bigsi/bigsi-service/pv-claim.json
 else
-  echo
   echo "Creating BIGSI service pv claim"
   echo
 
@@ -79,19 +71,15 @@ status_code=$(curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
     "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/api/v1/namespaces/$NAMESPACE/configmaps/mykrobe-atlas-bigsi-env" \
     -X GET -o /dev/null -w "%{http_code}")
 
-echo
 echo "BIGSI env config map status: $status_code"
-echo
 
 if [ $status_code == 200 ]; then
-  echo
   echo "Updating BIGSI env config map"
   echo
   curl -H 'Content-Type: application/strategic-merge-patch+json' -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
     "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/api/v1/namespaces/$NAMESPACE/configmaps/mykrobe-atlas-bigsi-env" \
     -X PATCH -d @k8/bigsi/bigsi-service/mykrobe-atlas-bigsi-env.json
 else
-  echo
   echo "Creating BIGSI env config map"
   echo
 
@@ -106,19 +94,15 @@ status_code=$(curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
     "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/api/v1/namespaces/$NAMESPACE/configmaps/mykrobe-atlas-bigsi-config" \
     -X GET -o /dev/null -w "%{http_code}")
 
-echo
 echo "BIGSI config config map status: $status_code"
-echo
 
 if [ $status_code == 200 ]; then
-  echo
   echo "Updating BIGSI config config map"
   echo
   curl -H 'Content-Type: application/strategic-merge-patch+json' -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
     "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/api/v1/namespaces/$NAMESPACE/configmaps/mykrobe-atlas-bigsi-config" \
     -X PATCH -d @k8/bigsi/bigsi-service/mykrobe-atlas-bigsi-config.json
 else
-  echo
   echo "Creating BIGSI config config map"
   echo
 
@@ -133,19 +117,15 @@ status_code=$(curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
     "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/apps/v1beta1/namespaces/$NAMESPACE/deployments/mykrobe-atlas-bigsi-deployment" \
     -X GET -o /dev/null -w "%{http_code}")
 
-echo
 echo "BIGSI service deployment status: $status_code"
-echo
 
 if [ $status_code == 200 ]; then
-  echo
   echo "Updating BIGSI service deployment"
   echo
   curl -H 'Content-Type: application/strategic-merge-patch+json' -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
     "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/apps/v1beta1/namespaces/$NAMESPACE/deployments/mykrobe-atlas-bigsi-deployment" \
     -X PATCH -d @k8/bigsi/bigsi-service/mykrobe-atlas-bigsi-deployment.json
 else
-  echo
   echo "Creating BIGSI service deployment"
   echo
 
