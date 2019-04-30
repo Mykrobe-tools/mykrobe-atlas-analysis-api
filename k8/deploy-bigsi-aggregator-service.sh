3bf868286afe7dfa1230d7a477cd727b705da61d
mthomsit <mark@makeandship.co.uk>
2019-04-30 10:36:55 +0100

    Bigsi aggregator

--- k8/deploy-bigsi-aggregator-service.sh
+++ k8/deploy-bigsi-aggregator-service.sh
@@ -55,49 +55,23 @@ echo
 # --------------------------------------------------------------
 
 status_code=$(curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
-    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/api/v1/namespaces/$NAMESPACE/configmaps/mykrobe-atlas-bigsi-config" \
-    -X GET -o /dev/null -w "%{http_code}")
-
-echo
-echo "BIGSI config config map status: $status_code"
-
-if [ $status_code == 200 ]; then
-  echo "Updating BIGSI config config map"
-  echo
-  curl -H 'Content-Type: application/strategic-merge-patch+json' -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
-    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/api/v1/namespaces/$NAMESPACE/configmaps/mykrobe-atlas-bigsi-config" \
-    -X PATCH -d @k8/bigsi/bigsi-service/mykrobe-atlas-bigsi-config.json
-else
-  echo "Creating BIGSI config config map"
-  echo
-
-  curl -H 'Content-Type: application/json' -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
-    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/api/v1/namespaces/$NAMESPACE/configmaps" \
-    -X POST -d @k8/bigsi/bigsi-service/mykrobe-atlas-bigsi-config.json
-fi
-
-echo
-
-# --------------------------------------------------------------
-
-status_code=$(curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
-    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/apps/v1beta1/namespaces/$NAMESPACE/deployments/mykrobe-atlas-bigsi-deployment" \
+    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/apps/v1beta1/namespaces/$NAMESPACE/deployments/mykrobe-atlas-bigsi-aggregator-api-deployment" \
     -X GET -o /dev/null -w "%{http_code}")
 echo
 
-echo "BIGSI service deployment status: $status_code"
+echo "BIGSI aggregator service deployment status: $status_code"
 
 if [ $status_code == 200 ]; then
-  echo "Updating BIGSI service deployment"
+  echo "Updating BIGSI aggregator service deployment"
   echo
   curl -H 'Content-Type: application/strategic-merge-patch+json' -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
-    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/apps/v1beta1/namespaces/$NAMESPACE/deployments/mykrobe-atlas-bigsi-deployment" \
-    -X PATCH -d @k8/bigsi/bigsi-service/mykrobe-atlas-bigsi-deployment.json
+    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/apps/v1beta1/namespaces/$NAMESPACE/deployments/mykrobe-atlas-bigsi-aggregator-api-deployment" \
+    -X PATCH -d @k8/bigsi/bigsi-aggregator-service/mykrobe-atlas-bigsi-aggregator-api-deployment.json
 else
-  echo "Creating BIGSI service deployment"
+  echo "Creating BIGSI aggregator service deployment"
   echo
 
   curl -H 'Content-Type: application/json' -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
     "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/apps/v1beta1/namespaces/$NAMESPACE/deployments" \
-    -X POST -d @k8/bigsi/bigsi-service/mykrobe-atlas-bigsi-deployment.json
+    -X POST -d @k8/bigsi/bigsi-aggregator-service/mykrobe-atlas-bigsi-aggregator-api-deployment.json
 fi
