1bdce3d39be709d2a56d57d2b0a4835b11a68400
Yassire Elhani <yassire@makeandship.com>
2019-05-02 11:00:48 +0100

    add analysis api ingress

--- k8/deploy-atlas.sh
+++ k8/deploy-atlas.sh
@@ -207,3 +207,30 @@ else
 fi
 
 echo
+
+# --------------------------------------------------------------
+
+status_code=$(curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
+    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/extensions/v1beta1/namespaces/$NAMESPACE/ingresses/analysis-api-ingress" \
+    -X GET -o /dev/null -w "%{http_code}")
+
+echo
+echo "Analysis Api Ingress: $status_code"
+
+if [ $status_code == 200 ]; then
+ echo "Updating ingress"
+ echo
+
+ curl -H 'Content-Type: application/strategic-merge-patch+json' -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
+    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/extensions/v1beta1/namespaces/$NAMESPACE/ingresses/analysis-api-ingress" \
+    -X PATCH -d @atlas-analysis-api-ingress.json
+else
+ echo "Creating ingress"
+ echo
+
+ curl -H 'Content-Type: application/json' -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
+    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/extensions/v1beta1/namespaces/$NAMESPACE/ingresses" \
+    -X POST -d @atlas-analysis-api-ingress.json
+fi
+
+echo
