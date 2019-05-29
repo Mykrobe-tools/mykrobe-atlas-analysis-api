7aecc68d783d8b8fc697422ab04322296e4f47e5
Phelim Bradley <wave@phel.im>
2019-05-29 15:05:53 +0100

    Update DistanceTaskManager

--- analyses/distance.py
+++ analyses/distance.py
@@ -7,6 +7,7 @@ import json
 import redis
 import operator
 from collections import OrderedDict
+import bitarray
 
 REDIS_HOST = os.environ.get("REDIS_HOST", "redis")
 REDIS_PORT = int(os.environ.get("REDIS_PORT", 6379))
@@ -18,7 +19,8 @@ INTERMEDIATE_RESULT_EXPIRY = 300
 
 def sort_and_filter_distance_dict(d, max_distance, limit):
     sorted_d = sorted(d.items(), key=operator.itemgetter(1))
-    sorted_d = [x for x in sorted_d if x[1] <= max_distance]
+    if max_distance:
+        sorted_d = [x for x in sorted_d if x[1] <= max_distance]
     if limit:
         sorted_d = sorted_d[:limit]
     return OrderedDict(sorted_d)
@@ -28,35 +30,62 @@ class DistanceTaskManager:
     def __init__(self, redis=REDIS, expiry=INTERMEDIATE_RESULT_EXPIRY):
         self.redis = redis
         self.expiry = expiry
-        self.samples = self.__get_samples()
+        self.samples = self.get_samples()
 
-    def __get_samples(self):
+    def get_samples(self):
         return {s.decode("utf-8") for s in self.redis.smembers(SAMPLES_KEY)}
 
     def __intermediate_key(self, s1, s2):
         return "_".join([str(s1), "xor", str(s2)])
 
+    def __distance_result_key(self, s1, s2):
+        return "_".join(["filtered", str(s1), "xor", str(s2)])
+
     def __genotype_bitarray_key(self, sample_id):
         return "_".join([sample_id, "genotypes"])
+
+    def __passed_bitarray_key(self, sample_id):
+        return "_".join([sample_id, "passed_filter"])
 
     def _build_xor(self, primary_sample, samples):
         primary_sample_key = self.__genotype_bitarray_key(primary_sample)
         pipe = self.redis.pipeline()
+        print(primary_sample_key, self.redis.bitcount(primary_sample_key))
         for secondary_sample in samples:
             secondary_sample_key = self.__genotype_bitarray_key(secondary_sample)
             k = self.__intermediate_key(primary_sample, secondary_sample)
-            if secondary_sample != primary_sample:
+            if (
+                secondary_sample != primary_sample
+            ):  # and (self.redis.bitcount(secondary_sample_key) > 100) and (self.redis.bitcount(primary_sample_key) > 100)
                 pipe.bitop("xor", k, primary_sample_key, secondary_sample_key)
                 pipe.expire(k, self.expiry)
+                ## Filter
+                primary_passed_filter_key = self.__passed_bitarray_key(primary_sample)
+                secondary_passed_filter_key = self.__passed_bitarray_key(
+                    secondary_sample
+                )
+                distance_result_key = self.__distance_result_key(
+                    primary_sample, secondary_sample
+                )
+                pipe.bitop(
+                    "and",
+                    distance_result_key,
+                    k,
+                    primary_passed_filter_key,
+                    secondary_passed_filter_key,
+                )
+                pipe.expire(distance_result_key, self.expiry)
         pipe.execute()
 
     def _count_xor(self, primary_sample, samples):
         samples = [s for s in samples if s != primary_sample]
         pipe = self.redis.pipeline()
         for secondary_sample in samples:
-            k = self.__intermediate_key(primary_sample, secondary_sample)
-            if secondary_sample != primary_sample:
-                pipe.bitcount(self.__intermediate_key(primary_sample, secondary_sample))
+            k = self.__distance_result_key(primary_sample, secondary_sample)
+            if secondary_sample != primary_sample and self.redis.exists(k):
+                pipe.bitcount(
+                    self.__distance_result_key(primary_sample, secondary_sample)
+                )
         res = pipe.execute()
         d = {}
         for q, diff in zip(samples, res):
@@ -64,10 +93,10 @@ class DistanceTaskManager:
         return d
 
     def distance(
-        self, primary_sample, max_distance=12, samples=None, limit=None, sort=True
+        self, primary_sample, max_distance=None, samples=None, limit=None, sort=True
     ):
         if samples is None:
-            samples = self.__get_samples()
+            samples = self.get_samples()
         if limit is not None:
             sort = True
         self._build_xor(primary_sample, samples)
@@ -90,20 +119,20 @@ class DistanceTaskManager:
             self._add_sample(sample_id)
             genotypes = self._create_genotype_bitarray(data["genotypes"])
             passed_filter = self._create_filtered_bitarray(data["filtered"])
-            filtered_genotypes = [
-                x for i, x in enumerate(genotypes) if passed_filter[i]
-            ]
-            self._insert_genotype_bitarray(filtered_genotypes, sample_id=sample_id)
+            self._insert_genotype_bitarray(genotypes, sample_id=sample_id)
+            self._insert_passed_bitarray(passed_filter, sample_id=sample_id)
 
-    def _insert_genotype_bitarray(self, bitarray, sample_id):
-        pipe = self.redis.pipeline()
-        for i, j in enumerate(bitarray):
-            pipe.setbit(self.__genotype_bitarray_key(sample_id), i, j)
-        pipe.execute()
+    def _insert_genotype_bitarray(self, ba, sample_id):
+        self.redis.set(self.__genotype_bitarray_key(sample_id), ba.tobytes())
+
+    def _insert_passed_bitarray(self, ba, sample_id):
+        self.redis.set(self.__passed_bitarray_key(sample_id), ba.tobytes())
 
     def _create_genotype_bitarray(self, sorted_calls):
-        bitarray = [int(call > 1) for call in sorted_calls]
-        return bitarray
+        int_array = [int(call > 1) for call in sorted_calls]
+        ba = bitarray.bitarray(int_array)
+        return ba
 
-    def _create_filtered_bitarray(self, sorted_calls):
-        return sorted_calls
+    def _create_filtered_bitarray(self, int_array):
+        ba = bitarray.bitarray(int_array)
+        return ba
