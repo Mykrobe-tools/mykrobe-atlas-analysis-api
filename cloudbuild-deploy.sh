414cf520528fe9b0198555e49cbaa7ca5a627e2a
Zhicheng-Liu <zl@ebi.ac.uk>
2020-07-13 17:48:42 +0100

    Fix analysis api and worker deployments

--- cloudbuild-deploy.sh
+++ cloudbuild-deploy.sh
@@ -2,7 +2,12 @@
 
 TOKEN=$(cat /workspace/ehk-token.txt)
 VERSION=$(cat /workspace/version.txt)
-curl -XPATCH 'https://45.86.170.176:6443/apis/apps/v1beta2/namespaces/mykrobe-dev/deployments/atlas-api-deployment' \
+curl -XPATCH 'https://45.86.170.176:6443/apis/apps/v1beta2/namespaces/mykrobe-dev/deployments/analysis-api' \
+-H "Authorization: Bearer $TOKEN" \
+-H 'Content-type: application/json-patch+json' \
+-d "[{ \"op\": \"replace\", \"path\": \"/spec/template/spec/containers/0/image\", \"value\": \"$VERSION\" }]" \
+--insecure
+curl -XPATCH 'https://45.86.170.176:6443/apis/apps/v1beta2/namespaces/mykrobe-dev/deployments/analysis-api-worker' \
 -H "Authorization: Bearer $TOKEN" \
 -H 'Content-type: application/json-patch+json' \
 -d "[{ \"op\": \"replace\", \"path\": \"/spec/template/spec/containers/0/image\", \"value\": \"$VERSION\" }]" \
