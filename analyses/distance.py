e1fd1cd3d90c1937dde37a53a2b9f1e71d5d0120
Zhicheng-Liu <zl@ebi.ac.uk>
2020-08-26 18:09:06 +0100

    Refactor distance job to use new distance request

--- analyses/distance.py
+++ analyses/distance.py
@@ -9,8 +9,7 @@ NUM_API_CLIENT_THREADS = 10
 configuration = Configuration()
 configuration.host = os.environ.get("ATLAS_DISTANCE_API", "http://distance-api-service/api/v1")
 api_client = distance_client.ApiClient(configuration=configuration, pool_threads=NUM_API_CLIENT_THREADS)
-samples_get_ids_api_instance = distance_client.SamplesGetIdsApi(api_client)
-neighbours_get_api_instance = distance_client.NeighboursGetApi(api_client)
+samples_get_api_instance = distance_client.SamplesGetApi(api_client)
 
 
 def _sort_and_filter_distance_results(results, max_distance, limit):
@@ -30,31 +29,31 @@ class DistanceTaskManager:
         if limit is not None:
             sort = True
         try:
-            neighbours = neighbours_get_api_instance.samples_id_nearest_neighbours_get(sample_id)
+            sample = samples_get_api_instance.samples_id_get(sample_id)
         except ApiException:
-            neighbours = []
+            sample = None
+
+        if not sample:
+            return {
+                "type": "distance",
+                "status": "error",
+                "leafId": "",
+                "result": []
+            }
+
+        neighbours = sample.nearest_neighbours
         if sort:
             neighbours = _sort_and_filter_distance_results(neighbours, max_distance, limit)
-        sample_ids = [neighbour.experiment_id for neighbour in neighbours]
-        sample_ids.append(sample_id)
-        try:
-            samples = samples_get_ids_api_instance.samples_get(ids=sample_ids)
-        except ApiException:
-            samples = []
 
-        if not samples:
-            return {}
-
-        leaves = {s.experiment_id: s.nearest_leaf_node.leaf_id for s in samples}
         nearest_neighbours = [{
             'sampleId': neighbour.experiment_id,
-            'leafId': leaves[neighbour.experiment_id],
+            'leafId': neighbour.leaf_id,
             'distance': neighbour.distance
         } for neighbour in neighbours]
 
         results = {
             'type': 'distance',
-            'leafId': leaves[sample_id],
+            'leafId': sample.nearest_leaf_node.leaf_id,
             'result': nearest_neighbours
         }
         return results
