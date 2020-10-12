6d05d1776447a630ff80c9504cd1a85cd47267a0
Giang Nguyen <giang@ebi.ac.uk>
2020-10-12 19:25:48 +0100

    Update default API root

--- analyses/tracking.py
+++ analyses/tracking.py
@@ -6,5 +6,5 @@ from tracking_client.configuration import Configuration
 NUM_API_CLIENT_THREADS = 10
 
 configuration = Configuration()
-configuration.host = os.environ.get("ATLAS_TRACKING_API", "http://distance-api-service/api/v1")
+configuration.host = os.environ.get("ATLAS_TRACKING_API", "http://tracking-api-service/api/v1")
 api_client = tracking_client.ApiClient(configuration=configuration, pool_threads=NUM_API_CLIENT_THREADS)
