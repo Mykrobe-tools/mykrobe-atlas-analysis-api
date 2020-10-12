de0cf7df55b75119eb4c105bfd249a25fee1a4f0
Giang Nguyen <giang.nghg@gmail.com>
2020-10-12 19:32:49 +0100

    Add tracking client (#26)
    * Add tracking API client
    
    * Rename
    
    * Untrack JetBrains IDE
    
    * Add biopython back
    
    * Remove unused class
    
    * Update Tracking client commit ref
    
    * Update default API root

---
+++ analyses/tracking.py
@@ -0,0 +1,10 @@
+import os
+
+import tracking_client
+from tracking_client.configuration import Configuration
+
+NUM_API_CLIENT_THREADS = 10
+
+configuration = Configuration()
+configuration.host = os.environ.get("ATLAS_TRACKING_API", "http://tracking-api-service/api/v1")
+api_client = tracking_client.ApiClient(configuration=configuration, pool_threads=NUM_API_CLIENT_THREADS)
