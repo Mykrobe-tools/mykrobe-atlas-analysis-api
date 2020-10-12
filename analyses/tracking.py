f2fbb6a2f36d97cde2eadd8099f4daf42f4d2600
Giang Nguyen <giang@ebi.ac.uk>
2020-10-12 19:05:29 +0100

    Remove unused class

--- analyses/tracking.py
+++ analyses/tracking.py
@@ -1,7 +1,6 @@
 import os
 
 import tracking_client
-from tracking_client.rest import ApiException
 from tracking_client.configuration import Configuration
 
 NUM_API_CLIENT_THREADS = 10
@@ -9,7 +8,3 @@ NUM_API_CLIENT_THREADS = 10
 configuration = Configuration()
 configuration.host = os.environ.get("ATLAS_TRACKING_API", "http://distance-api-service/api/v1")
 api_client = tracking_client.ApiClient(configuration=configuration, pool_threads=NUM_API_CLIENT_THREADS)
-
-
-class TrackingTaskManager:
-    pass
