0df9fd9fef37c2fbba437d67342c8f94f1dbd6f3
Giang Nguyen <giang@ebi.ac.uk>
2020-10-12 17:19:07 +0100

    Add tracking API client

---
+++ analyses/tracking.py
@@ -0,0 +1,15 @@
+import os
+
+import tracking_client
+from tracking_client.rest import ApiException
+from tracking_client.configuration import Configuration
+
+NUM_API_CLIENT_THREADS = 10
+
+configuration = Configuration()
+configuration.host = os.environ.get("ATLAS_TRACKING_API", "http://distance-api-service/api/v1")
+api_client = tracking_client.ApiClient(configuration=configuration, pool_threads=NUM_API_CLIENT_THREADS)
+
+
+class DistanceTaskManager:
+    pass
