a1ddfc3db45ff60d73e975dbae003cc968b7ee00
Giang Nguyen <giang@ebi.ac.uk>
2020-10-16 16:02:21 +0100

    Refactor

--- analyses/qc/fastq_qc.py
+++ analyses/qc/fastq_qc.py
@@ -1,9 +1,9 @@
 import os
 import subprocess
-from pathlib import Path
 
 from tracking_client import QcResult
 
+from helpers.bwa import map_reads
 from helpers.callers import het_snp_caller
 from helpers.grepers import grep_samstats
 
@@ -27,31 +27,6 @@ def fastq_qc(infile_path, sample_id, ref_path, outdir):
         decision=decision,
         number_of_het_snps=number_of_het_snps
     )
-
-
-def map_reads(infile_path, sample_id, reference_filepath, outdir, read_group=None):
-    """Ref: https://github.com/iqbal-lab-org/clockwork/blob/7113a9bfd67e1eb7ace4895a48c8e9a255a658e0/python/clockwork/read_map.py#L51
-    """
-    outpath = Path(outdir) / f'{sample_id}.sam'
-
-    # "LB:LIB" is needed, otherwise samtools rmdup segfaults when map_reads_set() is used
-    R_option = (
-        "" if read_group is None
-        else r"""-R '@RG\tLB:LIB\tID:"""
-             + read_group[0]
-             + r"""\tSM:"""
-             + read_group[1]
-             + "'"
-    )
-
-    with open(outpath, 'wb') as outfile:
-        subprocess.run([
-            "./bwa", "mem", "-M",
-            R_option,
-            reference_filepath, infile_path,
-        ], stdout=outfile)
-
-    return outpath
 
 
 def calculate_het_snps(sam_path, ref_path, outdir):
